9964ca13b77972d0c12344c7cdf0e92f548145a1
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-01-06 18:21:21 +0530

    Create CommandLine.java

---
+++ CommandLine.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class CommandLine{
+    public static void main(String args[]){
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int sum = a + b;
+
+        System.out.println("Addition of two command line no: "+sum);
+    }
+}
