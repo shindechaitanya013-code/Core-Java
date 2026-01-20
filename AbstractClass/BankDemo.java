affb7de2b43866f26bcc5a7fe78a6b27b9187d9b
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-01-20 20:39:09 +0530

    Add Bank Details using abstract class

---
+++ AbstractClass/BankDemo.java
@@ -0,0 +1,29 @@
+interface Bank {
+    void deposit(int amount);
+    void withdraw(int amount);
+}
+
+class Account implements Bank {
+    int balance = 5000;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    void display() {
+        System.out.println("Balance: " + balance);
+    }
+}
+
+public class BankDemo{
+    public static void main(String[] args) {
+        Account a = new Account();
+        a.deposit(2000);
+        a.withdraw(1000);
+        a.display();
+    }
+}
