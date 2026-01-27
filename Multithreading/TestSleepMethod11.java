578bd1b733fbf2fa8730af87f9c6c90e9d813c54
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-01-27 22:31:10 +0530

    Add Test sleep method

---
+++ Multithreading/TestSleepMethod11.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class TestSleepMethod1 extends Thread{  
+ 	public void run(){  
+  		for(int i=1;i<5;i++){  
+    			try{
+				Thread.sleep(500);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println(e);
+			}  
+    			System.out.println(i);
+  		}  
+ 	}  
+}
+
+class TestSleepMethod11{
+ 	public static void main(String args[]){  
+  		TestSleepMethod1 t1=new TestSleepMethod1();
+  		TestSleepMethod1 t2=new TestSleepMethod1();
+  			t1.start();
+  			t2.start();
+ 	}  
+}
