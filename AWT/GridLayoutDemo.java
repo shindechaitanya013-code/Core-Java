05432dfa3fc2bf82f309c7ca7fda52b9a85483ec
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-02-03 21:46:28 +0530

    Added AWT GridLayout demo

---
+++ AWT/GridLayoutDemo.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class GridLayoutDemo extends Frame{
+
+    public GridLayoutDemo(){
+        setLayout(new GridLayout(5,2));
+
+        Button b1 = new Button("A");
+        Button b2 = new Button("B");
+        Button b3 = new Button("C");
+        Button b4 = new Button("D");
+        Button b5 = new Button("E");
+        Button b6 = new Button("F");
+        Button b7 = new Button("G");
+        Button b8 = new Button("H");
+        Button b9 = new Button("I");
+        Button b10 = new Button("J");
+
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        add(b5);
+        add(b6);
+        add(b7);
+        add(b8);
+        add(b9);
+        add(b10);
+    }
+
+    public Insets getInsets(){
+        return new Insets(20,20,20,20);
+    }
+    public static void main(String args[]){
+        GridLayoutDemo gl = new GridLayoutDemo();
+        gl.setSize(300,200);
+        gl.setVisible(true);
+    }
+}
