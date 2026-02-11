c90d91e45d94402de6209c48273417a765e4f208
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-02-11 21:46:27 +0530

    Add program to iterate over ArrayList using for-each loop

---
+++ Collection/IerateArray.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class IerateArray{
+  public static void main(String[] args) {
+
+    ArrayList<String> animals = new ArrayList<>();
+    
+    animals.add("Cow");
+    animals.add("Cat");
+    animals.add("Dog");
+    
+    System.out.println("ArrayList: " + animals);
+
+    System.out.println("Accessing individual elements:  ");
+
+    for (String language : animals) {
+      System.out.print(language);
+      System.out.print(", ");
+    }
+  }
+}
