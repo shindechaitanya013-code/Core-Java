7065a1a82a97cd6108e4e6360bd10120eeaebac8
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-02-11 21:43:42 +0530

    Add Java program demonstrating ArrayList usage

---
+++ Collection/MainArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class MainArrayList{
+    public static void main(String args[]){
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Swaraj");
+        names.add("Om");
+        names.add("Raj");
+        names.add("Yash");
+        
+        System.out.println("ArrayList: "+names);
+    }
+}
