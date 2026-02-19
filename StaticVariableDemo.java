a00c990d99f53c8a0161ff4908ae12604b462c41
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-02-19 19:09:27 +0530

    Implement example showing default values of static variables

---
+++ StaticVariableDemo.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class sample{
+	static int i ; 
+    static int j = 2 ; 
+    static int k;
+	static String s1 ; static String s2 = "WElcome";
+	static boolean flag ;
+} 
+
+class StaticVariableDemo{
+	public static void main(String[] args){
+		System.out.println (sample.i);
+
+		System.out.println (sample.j);
+
+		System.out.println (sample.k);
+
+		System.out.println (sample.s1);
+
+		System.out.println (sample.s2);
+
+		System.out.println (sample.flag);
+	}
+} 
