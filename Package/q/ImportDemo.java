50f2e40ca3ac8674860b3bd2a4e45f5d1d4bf483
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-03-11 21:47:26 +0530

    Add Java package import demo with multiple packages (r, s, q)

---
+++ Package/q/ImportDemo.java
@@ -0,0 +1,16 @@
+package q;
+import r.R;
+import s.S;
+
+public class  ImportDemo
+{
+	public static void main (String args[]) 
+	{
+		R obj1= new R();
+		obj1.r1();
+
+		S obj2= new S();
+		obj2.s1();
+		System.out.println("Hello you finish ImportDemo !!");
+	}
+}
