e00cfc879486281a0d349e430ca6e0943fa366ee
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-03-12 21:29:48 +0530

    Create PackageDemo.java

---
+++ Package/PackageDemo.java
@@ -0,0 +1,17 @@
+import p1.B;
+import p1.Demo;
+import p2.C;
+
+public class PackageDemo 
+{
+public static void main(String args[])
+	{
+		B b =new B();
+		b.b1();
+		Demo d1=new Demo();
+		d1.demo();
+		C c =new C();
+		c.c1();
+	}
+
+}
