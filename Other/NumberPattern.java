699e0415228dd3f87ffbcfeab5892a0ed8daa2af
shindechaitanya013-code <shindechaitanya013@gmail.com>
2026-04-10 21:05:34 +0530

    Added Number Pattern program

--- Other/NumberPattern.java
+++ Other/NumberPattern.java
@@ -1,7 +1,8 @@
 class NumberPattern{
     public static void main(String args[]){
+        
         int num = 1;
-
+        
         for(int i=1;i<=4;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(num + " ");
